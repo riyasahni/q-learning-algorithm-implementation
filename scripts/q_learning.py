9184a3cbdc2e67f16137f86f024f5f7dc8e0e47f
riyasahni <sahnir@uchicago.edu>
2022-11-02 02:59:37 +0000

    pushing q learning code. still need to run and test lol.

--- scripts/q_learning.py
+++ scripts/q_learning.py
@@ -3,6 +3,8 @@
 import rospy
 import numpy as np
 import os
+import random
+import pandas as pd
 
 # Path of directory on where this file is located
 path_prefix = os.path.dirname(__file__) + "/action_states/"
@@ -44,10 +46,72 @@ class QLearning(object):
         self.states = np.loadtxt(path_prefix + "states.txt")
         self.states = list(map(lambda x: list(map(lambda y: int(y), x)), self.states))
 
+        # initializing empty q_matrix that is 's_t by a_t'
+        # fill it all with 0s since we don't know anything about the initial state of the matrix
+        # num rows in Q-matrix = number of states (rows of states matrix)
+        states = self.states.shape[0]
+        # num cols in Q-matrix = number of actions (rows of actions matrix --> 9)
+        actions = self.actions.shape[0]
+        # now initialize and return Q-matrix
+        # q_matrix =  [[0 for i in range(states)] for j in range(actions)]
+        self.q_matrix = np.zeros((states, actions), dtype = int)
+
+        # define the decay factor
+        self.decay = 0.8
+
+        # set topic name
+        # self.reward_topic = "reward"
+        # subscribe to the rewards ROS topic
+        rospy.Subscriber("/q_learning/reward", QLearningReward, self.update_q_matrix)
+
+#    def init_starting_q_matrix(self):
+
+    def update_q_matrix(self, data):
+        # convergence criteria: create a flag that becomes 'False'  every time q_matrix is updated
+        # once q_matrix has converged, then it will not be updated and converged flag will stay 'True'
+
+        # create a counter to count number of iterations we've gone through q_matrix to update it
+        iterations = 0
+        while (True):
+            converged = True
+            # iterate through each state
+            for s in range(0, states):
+                # select random value from 0 .. num_rows to end up in a random next state
+                # make sure the action taken to get to that next state is valid (cell val != -1)
+                while(True):
+                    rand_next_state = random.randint(0, states)
+                    if self.action_matrix[s][rand_next_state] != -1:
+                        # save the valid action
+                        action = self.action_matrix[s][rand_next_state]
+                        # break out of the regeneration loop
+                        break
+                # extract the reward from the ROS topic
+                reward = data
+                # find the q_matrix max val for the next state after the action
+                max_next_state_q_val = max(q_matrix[rand_next_state])
+                # find the cell in the q_matrix with corresponding state and valid action
+                # and update that cell. We leave alpha as 1.
+                initial_q_matrix_cell_val = self.q_matrix[s][action]
+                self.q_matrix[s][action] = reward + self.decay*max_next_state_q_val
+                # check if the updated cell has the same value as the original. If the
+                # cell value has changed, then the q_matrix has not converged yet...
+                if initial_q_matrix_cell_val != self.q_matrix[s][action]:
+                    converged = False
+            # update number of times we've updated entire q_matrix
+            iterations += 1
+            # check if no cells have been updated by the 10th iteration
+            if iterations >= 10 and converged:
+                # print message and break out of while loop if q_matrix is converged
+                print("q_matrix converged!")
+                break
+
     def save_q_matrix(self):
         # TODO: You'll want to save your q_matrix to a file once it is done to
         # avoid retraining
+        # use python built-in string to csv converter to convert matrix to csv file! :)
+        pd.DataFrame(self.q_matrix).to_csv('converged_q_matrix.csv')
         return
 
 if __name__ == "__main__":
     node = QLearning()
+
